e765c3b8d965559e3112fe34899b4247f8e06041
Andrei Muntean <andrei.muntean.dev@gmail.com>
2015-03-25 00:40:49 +0200

    Ayy lmao

---
+++ AyylmaoPrimes/AyylmaoPrimes.cpp
@@ -0,0 +1,15 @@
+#define lmao int
+#define ayyy using std::
+#define ayyyy lmao n;cin>>n;
+#define a(k) {lmao i=1; ayyyyyyyyyy(i, k)k)if(k%i==0)ayyyyyyy ayyyyyyyy; ayyyyyyyyy(k)}}
+#define ay ayyy cin;lmao k;ayyy cout;ayyy endl;
+#define ayy lmao main()
+#define ayyyyyyy goto
+#define ayyyyy lmao k;
+#define ayyyyyyyyy(aylmao) cout<<aylmao<<endl;
+#define ayyyyyyyyyy(ayylm, ao) for(ayylm=1;ayylm<ao;){while(++ayylm<
+#define ayyyyyy(n) ayyyyyyyyyy(k, n)=n)a(k) ayyyyyyyy:;}
+#define ayyylmao return 0;
+#define ayylmao ay ayy{ayyyy ayyyyy ayyyyyy(n);ayyylmao}
+#include<iostream>
+ayylmao
