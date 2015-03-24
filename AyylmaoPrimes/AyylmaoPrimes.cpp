63bc4fc9db9c3f1ad629078bd3e860c00d4f5dcb
Andrei Muntean <andrei.muntean.dev@gmail.com>
2015-03-25 00:58:27 +0200

    Ayy lmao

--- AyylmaoPrimes/AyylmaoPrimes.cpp
+++ AyylmaoPrimes/AyylmaoPrimes.cpp
@@ -1,15 +1,15 @@
 #define lmao int
 #define ayyy using std::
-#define ayyyy lmao n;cin>>n;
-#define a(k) {lmao i=1; ayyyyyyyyyy(i, k)k)if(k%i==0)ayyyyyyy ayyyyyyyy; ayyyyyyyyy(k)}}
-#define ay ayyy cin;lmao k;ayyy cout;ayyy endl;
+#define ayyyy lmao aaylmao;cin>>aaylmao;
+#define aaay {lmao aay=1; ayyyyyyyyyy(aay, aayy)aayy)if(aayy%aay==0)ayyyyyyy ayyyyyyyy; ayyyyyyyyy(aayy)}}
+#define ay ayyy cin;ayyy cout;ayyy endl;
 #define ayy lmao main()
 #define ayyyyyyy goto
-#define ayyyyy lmao k;
+#define ayyyyy lmao aayy;
 #define ayyyyyyyyy(aylmao) cout<<aylmao<<endl;
 #define ayyyyyyyyyy(ayylm, ao) for(ayylm=1;ayylm<ao;){while(++ayylm<
-#define ayyyyyy(n) ayyyyyyyyyy(k, n)=n)a(k) ayyyyyyyy:;}
+#define ayyyyyy ayyyyyyyyyy(aayy, aaylmao)=aaylmao)aaay ayyyyyyyy:;}
 #define ayyylmao return 0;
-#define ayylmao ay ayy{ayyyy ayyyyy ayyyyyy(n);ayyylmao}
+#define ayylmao ay ayy{ayyyy ayyyyy ayyyyyy;ayyylmao}
 #include<iostream>
 ayylmao
